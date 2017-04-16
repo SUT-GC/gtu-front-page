9fb7c47634678ef609e3e8d03fb990ceed6c84db
gc <gc@gcs-MacBook-Pro.local>
2017-04-16 16:38:36 +0800

    add all jq finish

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@ var list_info_url = service_url + "/listinfo/"
 var filter_left_select_universitys_url = service_url+"/gaoxiaofenshu/cdx"
 var filter_left_select_majors_url = service_url+"/zhuanye"
 var filter_left_select_score_url = service_url+"/gaoxiaofenshu/score"
+var filter_university_score_url = service_url+"/gaoxiaofenshu/university/score"
+var filter_major_score_url = service_url+"/zhuanye/majorscore"
+var filter_city_score_url = service_url+"/gaoxiaofenshu/cityscore"
+
+var search_university_majors = service_url+"/zhuanye/majorinuni/"
 
 var baidu_pake_url = "http://baike.baidu.com/item/"
 
@@ -20,7 +25,9 @@ hide_ids =  [
               "#major-limit",
               "#city-limit",
               "#result-nav",
-              "#gtu-body-foot",
+
+              "#gtu-body-foot-majors",
+              "#gtu-body-foot-history-score",
             ]
 $(document).ready(function(){
   showMyIds([])
@@ -35,6 +42,30 @@ $(document).ready(function(){
   $("#select-score-button").click(function(){
     queryScore($(this))
   })
+  $("#select-uni-score-button").click(function(){
+    queryUniScore($(this))
+  })
+  $("#select-major-score-button").click(function(){
+    queryMajorScore($(this));
+  })
+  $("#select-city-score-button").click(function(){
+    queryCityScore($(this));
+  })
+
+  show_has_majors_list = ["#select-university", "#select-score", "#select-major", "#score-limit", "#major-limit", "#city-limit", ]
+  show_history_score_list = ["#select-university", "#select-score", "#select-major", "#score-limit", "#major-limit", "#city-limit", ]
+
+  for(var i = 0; i < show_has_majors_list.length; i++){
+    $(show_has_majors_list[i]).on("click", ".has-majors-button", function(){
+      showHasMajors($(this))
+    })
+  }
+  for(var i = 0; i < show_history_score_list.length; i++){
+    $(show_history_score_list[i]).on("click", ".history-score-button", function(){
+      showHistoryScore($(this))
+    })
+  }
+
 });
 
 var showMyIds = function(args){
@@ -48,20 +79,23 @@ var showMyIds = function(args){
 
 var initAllList = function(){
   $.get(list_info_url, function(data, status){
-    majors = data.allMajor
-    citys = data.allCity
-    console.log(citys)
-    universitys = data.allUniversity
-    years = data.allYears
-    kebies = data.allKeBie
+    if (status === 'success') {
+      majors = data.allMajor
+      citys = data.allCity
+      console.log(citys)
+      universitys = data.allUniversity
+      years = data.allYears
+      kebies = data.allKeBie
 
-    initCitys()
-    initUniversitys()
-    initKebies()
-    initYears()
-    initMajors()
+      initCitys()
+      initUniversitys()
+      initKebies()
+      initYears()
+      initMajors()
+    }else{
+      alert("加在服务器资源失败")
+    }
   });
-  
 }
 
 var queryUniversitys = function(nowDocument){
@@ -92,7 +126,7 @@ var queryUniversitys = function(nowDocument){
       one_data = data[i]
       $(show_id).find("tbody")
         .append(
-          '<tr><th>'+i+'</th><th>'+one_data.universityName+'</th><th>'+one_data.kebie+'</th><th>'+one_data.cityName+'</th><th><a target="_blank" href="'+one_data.url+'">'+one_data.universityName+'</a></th><th><button type="button" class="btn btn-success result-button">所设专业</button></th><th><button type="button" class="btn btn-success result-button">历年分数线</button></th></tr>'
+          '<tr><th>'+i+'</th><th class="universityName-th">'+one_data.universityName+'</th><th class="kebie-th">'+one_data.kebie+'</th><th class="cityName-th">'+one_data.cityName+'</th><th><a target="_blank" href="'+one_data.url+'">'+one_data.universityName+'</a></th><th><button type="button" class="btn btn-success result-button has-majors-button">所设专业</button></th><th><button type="button" class="btn btn-success result-button history-score-button">历年分数线</button></th></tr>'
         )
     }
     showMyIds(["#gtu-body-result", show_id, ])
@@ -120,7 +154,7 @@ var queryMajors = function(nowDocument){
       one_data = data[i]
       $(show_id).find("tbody")
         .append(
-          '<tr><th>'+i+'</th><th>'+one_data.majorName+'</th><th><a target="_blank" href="'+baidu_pake_url+one_data.universityName+'">'+one_data.universityName+'</a></th><th>'+one_data.cityName+'</th><th><a target="_blank" href="'+one_data.majorUrl+'">'+one_data.majorName+'</a></th><th><button type="button" class="btn btn-success result-button">历年分数线</button></th></tr>'
+          '<tr><th>'+i+'</th><th>'+one_data.majorName+'</th><th class="universityName-th"><a target="_blank" href="'+baidu_pake_url+one_data.universityName+'">'+one_data.universityName+'</a></th><th class="kebie-th">'+one_data.kebie+'</ht><th class="cityName-th">'+one_data.cityName+'</th><th><a target="_blank" href="'+one_data.majorUrl+'">'+one_data.majorName+'</a></th><th><button type="button" class="btn btn-success result-button history-score-button">历年分数线</button></th></tr>'
         )
     }
     showMyIds(["#gtu-body-result", show_id, ]) 
@@ -157,10 +191,171 @@ var queryScore = function(nowDocument){
       one_data = data[i]
       $(show_id).find("tbody")
         .append(
-          '<tr><th>'+i+'</th><th>'+one_data.universityName+'</th><th>'+one_data.kebie+'</th><th>'+one_data.cityName+'</th><th>'+one_data.year+'</th><th>'+one_data.pici+'</th><th>'+one_data.scoreLimit+'</th><th><a target="_blank" href="'+one_data.url+'">'+one_data.universityName+'</a></th><th><button type="button" class="btn btn-success result-button">所设专业</button></th><th><button type="button" class="btn btn-success result-button">历年分数线</button></th></tr>'
+          '<tr><th>'+i+'</th><th class="universityName-th">'+one_data.universityName+'</th><th class="kebie-th">'+one_data.kebie+'</th><th class="cityName-th">'+one_data.cityName+'</th><th>'+one_data.year+'</th><th>'+one_data.pici+'</th><th>'+one_data.scoreLimit+'</th><th><a target="_blank" href="'+one_data.url+'">'+one_data.universityName+'</a></th><th><button type="button" class="btn btn-success result-button has-majors-button">所设专业</button></th><th><button type="button" class="btn btn-success result-button history-score-button">历年分数线</button></th></tr>'
         )
     }
     showMyIds(["#gtu-body-result", show_id, ]) 
+  })
+}
+
+var queryUniScore = function(nowDocument){
+  var parentDocument = nowDocument.parent().parent()
+  var cityName = parentDocument.find(".all_citys").val()
+  var kebie = parentDocument.find(".all_kebies").val()
+  var universityName = parentDocument.find(".all_universitys").val()
+  
+  if (!checkCity(cityName)) {
+    return
+  }
+  if (!checkKebie(kebie)) {
+    return
+  }
+  if(!checkUniversity(universityName)){
+    return
+  }
+
+  $.get(filter_university_score_url+"?cityName="+cityName+"&kebie="+kebie+"&universityName="+universityName, function(data, status){
+    console.log(data, status)
+    if (status != "success") {
+      alert("查询失败, 服务异常!")
+      return
+    }
+    show_id = "#score-limit"
+    $(show_id).find("tbody").empty()
+    for(var i = 0; i < data.length; i++){
+      one_data = data[i]
+      $(show_id).find("tbody")
+        .append(
+          '<tr><th>'+i+'</th><th class="universityName-th">'+one_data.universityName+'</th><th class="kebie-th">'+one_data.kebie+'</th><th class="cityName-th">'+one_data.cityName+'</th><th>'+one_data.year+'</th><th>'+one_data.pici+'</th><th>'+one_data.scoreLimit+'</th><th><a target="_blank" href="'+one_data.url+'">'+one_data.universityName+'</a></th><th><button type="button" class="btn btn-success result-button has-majors-button">所设专业</button></th><th><button type="button" class="btn btn-success result-button history-score-button">历年分数线</button></th></tr>'
+        )
+    }
+    showMyIds(["#gtu-body-result", show_id, ]) 
+  })
+}
+
+var queryMajorScore = function(nowDocument){
+  var parentDocument = nowDocument.parent().parent()
+  var majorName = parentDocument.find(".all_majors").val()
+  var cityName = parentDocument.find(".all_citys").val()
+  var year = parentDocument.find(".all_years").val()
+  
+  if (!checkMajor(majorName)) {
+    return
+  }
+  if (!checkCity(cityName)) {
+    return
+  }
+  if(!checkYear(year)){
+    return
+  }
+
+  $.get(filter_major_score_url+"?majorName="+majorName+"&cityName="+cityName+"&year="+year, function(data, status){
+    console.log(data, status)
+    if (status != "success") {
+      alert("查询失败, 服务异常!")
+      return
+    }
+    show_id = "#major-limit"
+    $(show_id).find("tbody").empty()
+    for(var i = 0; i < data.length; i++){
+      one_data = data[i]
+      $(show_id).find("tbody")
+        .append(
+          '<tr><th>'+i+'</th><th>'+one_data.majorName+'</th><th class="universityName-th">'+one_data.universityName+'</th><th class="kebie-th">'+one_data.kebie+'</th><th class="cityName-th">'+one_data.cityName+'</th><th>'+one_data.year+'</th><th>'+one_data.pici+'</th><th>'+one_data.avgScore+'</th><th><a target="_blank" href="'+one_data.url+'">'+one_data.majorName+'</a></th><th><button type="button" class="btn btn-success result-button has-majors-button">所设专业</button></th><th><button type="button" class="btn btn-success result-button history-score-button">历年分数线</button></th></tr>'
+        )
+    }
+    showMyIds(["#gtu-body-result", show_id, ]) 
+  })
+}
+
+var queryCityScore = function(nowDocument){
+  var parentDocument = nowDocument.parent().parent()
+  var cityName = parentDocument.find(".all_citys").val()
+  var kebie = parentDocument.find(".all_kebies").val()
+  var year = parentDocument.find(".all_years").val()
+  
+  if (!checkCity(cityName)) {
+    return
+  }
+  if (!checkKebie(kebie)) {
+    return
+  }
+  if(!checkYear(year)){
+    return
+  }
+
+  $.get(filter_city_score_url+"?cityName="+cityName+"&keBie="+kebie+"&year="+year, function(data, status){
+    console.log(data, status)
+    if (status != "success") {
+      alert("查询失败, 服务异常!")
+      return
+    }
+    show_id = "#city-limit"
+    $(show_id).find("tbody").empty()
+    for(var i = 0; i < data.length; i++){
+      one_data = data[i]
+      $(show_id).find("tbody")
+        .append(
+          '<tr><th>'+i+'</th><th class="universityName-th">'+one_data.universityName+'</th><th class="kebie-th">'+one_data.kebie+'</th><th class="cityName-th">'+one_data.cityName+'</th><th>'+one_data.year+'</th><th>'+one_data.pici+'</th><th>'+one_data.scoreLimit+'</th><th><a target="_blank" href="'+one_data.url+'">'+one_data.universityName+'</a></th><th><button type="button" class="btn btn-success result-button has-majors-button">所设专业</button></th><th><button type="button" class="btn btn-success result-button history-score-button">历年分数线</button></th></tr>'
+        )
+    }
+    showMyIds(["#gtu-body-result", show_id, ]) 
+  })
+}
+
+var showHasMajors = function(nowDocument){
+  var parentDocument = nowDocument.parent().parent()
+  var universityName = parentDocument.find(".universityName-th").text()
+  var kebie = parentDocument.find(".kebie-th").text()
+  var cityName = parentDocument.find(".cityName-th").text()
+  
+  $.get(search_university_majors+universityName, function(data, status){
+    console.log(data, status)
+    if (status != "success") {
+      alert("查询失败, 服务异常!")
+      return
+    }
+
+    show_id = "#gtu-body-foot-majors"
+
+    $(show_id).find("tbody").empty()
+    for(var i = 0; i < data.length; i++){
+      one_data = data[i]
+      $(show_id).find("tbody")
+        .append(
+          '<tr><th>'+i+'</th><th>'+one_data.universityName+'</th><th><a target="_blank" href="'+one_data.majorUrl+'">'+one_data.majorName+'</a></th><th>'+one_data.kebie+'</th><th>'+one_data.cityName+'</th><th>'+one_data.year+'</th><th>'+one_data.pici+'</th><th>'+one_data.avgScore+'</th></tr>'
+        )
+    }
+
+    showMyIds([show_id, ]) 
+  })
+}
+
+var showHistoryScore = function(nowDocument){
+  var parentDocument = nowDocument.parent().parent()
+  var universityName = parentDocument.find(".universityName-th").text()
+  var kebie = parentDocument.find(".kebie-th").text()
+  var cityName = parentDocument.find(".cityName-th").text()
+  
+  $.get(filter_university_score_url+"?cityName="+cityName+"&kebie="+kebie+"&universityName="+universityName, function(data, status){
+    console.log(data, status)
+    if (status != "success") {
+      alert("查询失败, 服务异常!")
+      return
+    }
+
+    show_id = "#gtu-body-foot-history-score"
+
+    $(show_id).find("tbody").empty()
+    for(var i = 0; i < data.length; i++){
+      one_data = data[i]
+      $(show_id).find("tbody")
+        .append(
+          '<tr><th>'+i+'</th><th>'+one_data.universityName+'</th><th>'+one_data.kebie+'</th><th>'+one_data.cityName+'</th><th>'+one_data.year+'</th><th>'+one_data.pici+'</th><th>'+one_data.scoreLimit+'</th><th><a target="_blank" href="'+one_data.url+'">'+one_data.universityName+'</a></th></tr>'
+        )
+    }
+
+    showMyIds([show_id, ]) 
   })
 }
 
